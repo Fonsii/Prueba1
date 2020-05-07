37bc95a4ec1348572aed8473e6db130eb55c7086
BASM7 <j.alonsomm07@gmail.com>
2020-05-06 22:06:09 -0600

    Avance en la clase Menu para el manejo de entradas del usuario.

--- Menu.cpp
+++ Menu.cpp
@@ -1,8 +1,35 @@
 #include "Menu.h"
+#include <iostream>
 
 Menu::Menu()
-{
+{}
+
+Menu::~Menu() {}
+
+int Menu::getUserEntryOption(std::string message) {
+	int reply = -1;
+	bool validEntry = false;
+	while (!validEntry) {
+		std::cout << "\n" << message << std::endl;		
+		std::cin >> reply;
+
+		if (reply != -1) {
+			validEntry = true;
+		}
+	}
+	return reply;
 }
 
-
+std::string Menu::getUserEntryText(std::string message) {
+	std::string reply;
+	bool validEntry = false;
+	while (!validEntry) {
+		std::cout << "\n" << message << std::endl;
+		std::cin >> reply;
+		if (!reply.empty()) {
+			validEntry = true;
+		}
+	}
+	return reply;
+}
 
--- Menu.h
+++ Menu.h
@@ -1,10 +1,12 @@
 #pragma once
-
+#include <string>
 
 class Menu {
 
 public:
 	Menu();
+	~Menu();
 
-
+	int getUserEntryOption(std::string message); // pide al usuario ingresar una opción númerica.
+	std::string getUserEntryText(std::string message); // pide al usuario ingresar texto.
 };
