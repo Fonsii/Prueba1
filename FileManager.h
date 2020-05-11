70b5aa0aa7d9e68a190595ff97bcff842e847695
VicMA <vmora@DESKTOP-1M0S9I5>
2020-05-11 12:21:55 -0600

    Se modifica readOcurenceFile

--- FileManager.h
+++ FileManager.h
@@ -13,5 +13,5 @@ public:
 	std::string readFromFile(std::string);
 	void writeToFile(std::string, std::vector<std::string>);
 	void saveTree(std::string, std::string);
-	std::vector<std::string>readOcurrenceFile(std::string filename);
+	std::vector<std::string>readOcurrenceFile(std::string,std::vector<int>);
 };
